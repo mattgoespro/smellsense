92b69baad9a36a834a4feb2c4e73935774d63eb0
Matt Young <mattyoung305@gmail.com>
2022-02-03 13:12:31 +0200

    cp

--- tools/scripts/static_code_analyze.sh
+++ tools/scripts/static_code_analyze.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
 
-ERRORS="";
 EXIT=0;
+BUILD=$1;
+ERRORS="";
 
-OUTPUT=$(grep -r "//String get bannerAdUnitId => BannerAd.testAdUnitId");
-
-if [[ -n "$OUTPUT" ]]; then
-    ERRORS="$ERRORS\nError: Test ads must be enabled in debug builds.";
-    EXIT=1;
-fi
-
-OUTPUT=$(grep -r "static const timerDuration = 1;");
-
-if [[ -n "$OUTPUT" ]]; then
-    ERRORS="$ERRORS\nError: Training timer time is in debug mode.";
-    EXIT=1;
+if [ "$BUILD" == "production" ]; then
+    OUTPUT=$(grep -r "//String get bannerAdUnitId => BannerAd.testAdUnitId");
+    
+    if [[ -n "$OUTPUT" ]]; then
+        ERRORS="$ERRORS\nError: Test ads must be enabled in debug builds.";
+        EXIT=1;
+    fi
+    
+    OUTPUT=$(grep -r "static const timerDuration = 1;");
+    
+    if [[ -n "$OUTPUT" ]]; then
+        ERRORS="$ERRORS\nError: Training timer time is in debug mode.";
+        EXIT=1;
+    fi
+elif [ "$BUILD" == "development" ]; then
+    OUTPUT=$(grep -r "//String get bannerAdUnitId => this._bannerAdUnitId;");
+    
+    if [[ -n "$OUTPUT" ]]; then
+        ERRORS="$ERRORS\nError: Ads not enabled.";
+        EXIT=1;
+    fi
+    
+    OUTPUT=$(grep -r "static const timerDuration = 15;");
+    
+    if [[ -n "$OUTPUT" ]]; then
+        ERRORS="$ERRORS\nError: Incorrect default training time.";
+        EXIT=1;
+    fi
+else
+    echo "Error: Build type is required."
 fi
 
 printf "$ERRORS";
+
+printf "\n\nExit code $EXIT.";
 
 exit $EXIT;

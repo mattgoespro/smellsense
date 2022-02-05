000dca3af9994c4a2be6be8f29789094de2b7252
Matt Young <mattyoung305@gmail.com>
2022-02-05 20:29:38 +0200

    cp

--- refresh_build.sh
+++ refresh_build.sh
@@ -6,5 +6,3 @@ flutter clean
 flutter pub get
 flutter pub run flutter_native_splash:create --path=tools/flutter-dev/flutter_native_splash.yaml
 flutter pub run flutter_launcher_icons:main -f tools/flutter-dev/flutter_launcher_icons.yaml
-
-
